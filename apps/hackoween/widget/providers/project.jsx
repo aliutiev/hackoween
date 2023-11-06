98afc15ee6e358a6f15c13a89ad2dadbe7e9abc0
andrew <a.liutiev@gmail.com>
2023-11-06 18:36:31 +0000

    adding changes and pushing new file to BOS

--- apps/hackoween/widget/providers/project.jsx
+++
@@ -1,5 +0,0 @@
-const projects = VM.require("hackoween.near/widget/data.projects") || [];
-
-const Children = props.children;
-
-return <Children projects={projects} />;
---
+++ apps/sample/widget/button_general.jsx
@@ -0,0 +1,10 @@
+const value = props.value || "no-name button";
+const handleButtonClick =
+  props.handleButtonClick || (() => console.log("button clicked"));
+
+return (
+  <button class="btn btn-primary" onClick={handleButtonClick}>
+    {value}
+    {"test value"}
+  </button>
+);
